edba8dbd99dae534eaaba42159f065b0d5aac8d8
fishribs <fishribs@3756ed7c-b672-11dd-b040-2d45f096adfe>
2008-12-04 15:57:10 +0000

    Add entities to the app and add a one-many relationship (Album-> Songs)

--- trunk/myTunes/src/com/door/mytunes/Album.java
+++ trunk/myTunes/src/com/door/mytunes/Album.java
@@ -1,5 +1,6 @@
 package com.door.mytunes;
 
+import com.u2d.list.RelationalList;
 import com.u2d.type.atom.ImgEO;
 import com.u2d.model.AbstractComplexEObject;
 import com.u2d.model.Title;
@@ -15,8 +16,10 @@ public class Album extends AbstractComplexEObject {
 
     private final StringEO _name = new StringEO();
     private final ImgEO _cover = new ImgEO();
-    
+    private final RelationalList _songs = new RelationalList(Song.class);
+    public static final Class songsType = Song.class;
     public static final String[] fieldOrder = {"name", "cover"};
+    public static final String songsInverseFieldName = "album";
 
     public Album() {
     }
@@ -31,5 +34,9 @@ public class Album extends AbstractComplexEObject {
 
     public Title title() {
         return _name.title();
+    }
+
+    public RelationalList getSongs() {
+        return _songs;
     }
 }

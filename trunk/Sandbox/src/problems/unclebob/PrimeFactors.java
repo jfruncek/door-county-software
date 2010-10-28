c1b08c462a7eeaae3cc8bf6b8f3c8c486183e9dd
jfruncek <jfruncek@3756ed7c-b672-11dd-b040-2d45f096adfe>
2010-10-28 21:14:40 +0000

    Add some tests

--- trunk/Sandbox/src/problems/unclebob/PrimeFactors.java
+++ trunk/Sandbox/src/problems/unclebob/PrimeFactors.java
@@ -5,8 +5,19 @@ import java.util.List;
 
 public class PrimeFactors {
 
+    static List<Integer> factors = new ArrayList<Integer>();
+    
 	public static List<Integer> generate(int i) {
-		return new ArrayList<Integer>();
+	    factors.add(1);
+	    if ( isPrime(i) ) {
+	        factors.add(i);
+	    }
+		return factors;
 	}
+
+    private static boolean isPrime(int i) {
+        if ( i == 2 ) { return true; };
+        return false;
+    }
 
 }
--- trunk/Sandbox/test/problems/unclebob/PrimeFactorsTest.java
+++ trunk/Sandbox/test/problems/unclebob/PrimeFactorsTest.java
@@ -1,15 +1,24 @@
 package problems.unclebob;
 
-import java.util.Arrays;
 import java.util.List;
 
 import junit.framework.TestCase;
 
 public class PrimeFactorsTest extends TestCase {
 
-	public void testGenerates() {
-		List<Integer> factors = PrimeFactors.generate(10);
-		assertTrue("generates prime factors", Arrays.equals(new Integer[] {1, 2, 5}, factors.toArray(new Integer[] {})));
+	public void testGeneratesFactorsOf2() {
+		List<Integer> factors = PrimeFactors.generate(2);
+        assertEquals("generates how many prime factors?", 2, factors.size());
+		assertTrue("generates a prime factor, 1", factors.contains(1));
+        assertTrue("generates a prime factor, 2", factors.contains(2));
 	}
-
+	
+	public void testGeneratesFactorsOf10() {
+        List<Integer> factors = PrimeFactors.generate(10);
+        assertEquals("generates how many prime factors?", 3, factors.size());
+        assertTrue("generates a prime factor, 1", factors.contains(1));
+        assertTrue("generates a prime factor, 2", factors.contains(2));
+        assertTrue("generates a prime factor, 5", factors.contains(5));
+    }
+    
 }

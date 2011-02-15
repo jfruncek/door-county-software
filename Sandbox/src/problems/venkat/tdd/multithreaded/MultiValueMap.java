327ec70308b729d6e531983aaa56fc61f2f360b7
jfruncek <jfruncek@3756ed7c-b672-11dd-b040-2d45f096adfe>
2011-02-15 23:05:14 +0000

    Finished Venket's example. Wow, that's way too easy.

--- Sandbox/src/problems/venkat/tdd/multithreaded/MultiValueMap.java
+++ Sandbox/src/problems/venkat/tdd/multithreaded/MultiValueMap.java
@@ -5,10 +5,13 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MultiValueMap<K, V> {
 
     Map<K, List<V>> _map = new HashMap<K, List<V>>();
+  private Lock _lock = new ReentrantLock();
     
     public int getSize() {
         return _map.size();
@@ -22,9 +25,22 @@ public class MultiValueMap<K, V> {
     }
 
     public void put(K key, V value) {
-        List<V> values = new ArrayList<V>();
+        _lock.lock();
+        try {
+            putValue(key, value);
+        } finally {
+            _lock.unlock();
+        }
+    }
+
+    protected void putValue(K key, V value) {
+        List<V> values = Collections.synchronizedList(new ArrayList<V>());
         if (_map.containsKey(key)) values = _map.get(key);
         values.add(value);
         _map.put(key, values);
+    }
+
+    protected void setLock(Lock lock) {
+        _lock = lock;
     }
 }
--- Sandbox/test/problems/venkat/tdd/multithreaded/MultiValueMapTest.java
+++ Sandbox/test/problems/venkat/tdd/multithreaded/MultiValueMapTest.java
@@ -1,6 +1,8 @@
 package problems.venkat.tdd.multithreaded;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 import junit.framework.TestCase;
 
@@ -23,14 +25,86 @@ public class MultiValueMapTest extends TestCase {
     }
     
     public void testAddValueToMap() {
-        _map.put("", "");
+        _map.put(KEY, "v1");
         assertNotNull(_map.get(""));
-        assertEquals("map value list size:", 1, _map.get("").size());
+        assertEquals("map value list size:", 1, _map.get(KEY).size());
+        assertEquals("v1", _map.get(KEY).get(0));
     }
     
     public void testAddMultiValueToMap() {
         _map.put(KEY, "v1");
         _map.put(KEY, "v2");
         assertEquals("map value list size:", 2, _map.get(KEY).size());
+        assertEquals("v1", _map.get(KEY).get(0));
+        assertEquals("v2", _map.get(KEY).get(1));
     }
+    
+    public void testCheckSizeAfterPuts() {
+        _map.put("1", "one");
+        _map.put("2", "two");
+        _map.put("1", "uno");
+
+        assertEquals(2, _map.getSize());
+    }
+  
+  public void testEnsureGetValuesReturnsSynchronizedList() {
+        _map.put("1", "one");
+        List<String> values = _map.get("1");
+    
+        assertEquals(Collections.synchronizedList(values).getClass(), values.getClass());
+  }
+  
+  class MockLock extends ReentrantLock {
+    private static final long serialVersionUID = 1L;
+    public boolean locked;
+    public boolean unlocked;
+
+    @Override public void lock() {
+      locked = true;
+    }
+
+    @Override public void unlock() {
+      unlocked = true;
+    }
+  }
+  
+  public void testPutIsMutuallyExclusive() {
+    final MockLock mockLock = new MockLock();
+    MultiValueMap<String, String> map = new MultiValueMap<String, String>() {
+      @Override protected void putValue(String key, String value) {
+        assertTrue(mockLock.locked);
+        assertFalse(mockLock.unlocked);
+      }
+    };
+    map.setLock(mockLock);
+
+    assertFalse(mockLock.locked);
+    assertFalse(mockLock.unlocked);
+
+    map.put("3", "three");
+
+    assertTrue(mockLock.locked);
+    assertTrue(mockLock.unlocked);
+  }
+  
+  public void testPutLockAndUnlockAreGuardedByTryFinally() {
+    MultiValueMap<String, String> map = new MultiValueMap<String, String>() {
+      @Override protected void putValue(String key, String value) {
+        throw new RuntimeException("simulated exception");
+      }
+    };
+
+    MockLock mockLock = new MockLock();
+    map.setLock(mockLock);
+
+    try {
+      map.put("4", "four");
+      fail("Expected the simulated exception!");
+    } catch(RuntimeException ex) {
+      // :) Expected
+    }
+
+    assertTrue(mockLock.locked);
+    assertTrue(mockLock.unlocked);
+  }
 }

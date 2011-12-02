bb4f8445ea6dae62488b5a7f5e3c20e4ba16cd9b
jfruncek <jfruncek@3756ed7c-b672-11dd-b040-2d45f096adfe>
2011-12-02 19:48:59 +0000

    switch to simpler project structure

---
+++ trunk/or-parent/doc/Jars_added/mvn-install-jars.sh
@@ -0,0 +1,14 @@
+echo installing jars- Maven issues
+
+mvn install:install-file -DgroupId=javax.jms -DartifactId=jms -Dversion=1.1 -Dpackaging=jar -Dfile=jms.jar
+mvn install:install-file -DgroupId=com.sun.jdmk -DartifactId=jmxtools -Dversion=1.2.1 -Dpackaging=jar -Dfile=jmxtools.jar
+mvn install:install-file -DgroupId=com.sun.jmx -DartifactId=jmxri -Dversion=1.2.1 -Dpackaging=jar -Dfile=jmxri.jar
+mvn install:install-file -DgroupId=xmlbeans -DartifactId=xmlbeans -Dversion=2.4.0 -Dpackaging=jar -Dfile=xmlbeans-2.4.0.jar
+
+echo installing jars- Open Reports issues
+
+mvn install:install-file -DgroupId=net.socialchange.doctype -DartifactId=doctype-changer -Dversion=1.1 -Dpackaging=jar -Dfile=DoctypeChanger.jar
+mvn install:install-file -DgroupId=net.sf.docbook -DartifactId=docbook -Dversion=1.74.0 -Dpackaging=jar -Dfile=docbook.jar
+
+mvn install:install-file -DgroupId=com.googlcode.strut2gwtplugin -DartifactId=struts2gwtplugin -Dversion=1.02 -Dpackaging=jar -Dfile=struts2gwtplugin-1.02.jar
+mvn install:install-file -DgroupId=javassist -DartifactId=javassist -Dversion=3.12.1.GA -Dpackaging=jar -Dfile=javassist-3.12.1.GA.jar
